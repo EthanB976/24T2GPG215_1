72d0a19c79e52d718190539168c15bea68d89558
SA <1032124@student.sae.edu.au>
2024-07-11 13:53:12 +1000

    e

--- Assets/Scripts/Wallspike.cs
+++ Assets/Scripts/Wallspike.cs
@@ -18,7 +18,7 @@ public class Wallspike : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        p = GameObject.Find("PlayerControl").GetComponent<PlayerMovement>();
+       
         if (dir)
         {
             for (int i = 0; i < amount; i++)
