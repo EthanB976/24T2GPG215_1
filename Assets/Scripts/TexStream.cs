e92cb9e56df13e3f6b18a2978a514e842bf7c73a
SA <1032124@student.sae.edu.au>
2024-07-12 13:57:23 +1000

    Added texture streaming background
    yeah

---
+++ Assets/Scripts/TexStream.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO.Enumeration;
+using UnityEngine;
+using System.IO;
+
+public class TexStream : MonoBehaviour
+{
+    [SerializeField] SpriteRenderer s;
+    public string filename = "pSprite";
+    public string filetype = ".png";
+    public string filepath = Application.streamingAssetsPath;
+    private string fullFilePath;
+    // Start is called before the first frame update
+    void Start()
+    {
+        s = GetComponent<SpriteRenderer>();
+       fullFilePath = Path.Combine(filepath, filename + filetype);
+        LoadTex();
+
+    }
+
+    void LoadTex()
+    {
+        if(File.Exists(fullFilePath))
+        {
+            byte[] imagebytes = File.ReadAllBytes(fullFilePath);
+
+            Texture2D tex  = new Texture2D(2,2);
+            tex.LoadImage(imagebytes);
+            Rect r = new Rect(gameObject.transform.position.x,gameObject.transform.position.y,tex.width,tex.height);
+           s.sprite = Sprite.Create(tex, r, Vector2.zero);
+        }
+        else
+        {
+            {
+                Debug.Log("File doesn't exist at " + fullFilePath);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
